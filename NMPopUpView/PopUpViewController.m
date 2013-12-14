569f1f0b5169062527c6f5c40ac5297b5a90d0bd
Nikos Maounis <nikospsy2k@gmail.com>
2013-12-14 09:20:46 +0200

    Added iPad support
    Added iPad support

--- NMPopUpView/PopUpViewController.m
+++ NMPopUpView/PopUpViewController.m
@@ -17,6 +17,7 @@
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    
     if (self) {
         // Custom initialization
     }
--- NMPopUpView/ViewController.m
+++ NMPopUpView/ViewController.m
@@ -23,10 +23,17 @@
 }
 
 - (IBAction)showPopUp:(id)sender {
-    self.popViewController = [[PopUpViewController alloc] initWithNibName:@"PopUpViewController" bundle:nil];
-    [self.popViewController setTitle:@"This is a popup view"];
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad){
+        self.popViewController = [[PopUpViewController alloc] initWithNibName:@"PopUpViewController_iPad" bundle:nil];
+        [self.popViewController setTitle:@"This is a popup view"];
     
-    [self.popViewController showInView:self.view animated:YES];
+        [self.popViewController showInView:self.view animated:YES];
+    } else {
+        self.popViewController = [[PopUpViewController alloc] initWithNibName:@"PopUpViewController" bundle:nil];
+        [self.popViewController setTitle:@"This is a popup view"];
+        
+        [self.popViewController showInView:self.view animated:YES];
+    }
 }
 
 - (void)didReceiveMemoryWarning
