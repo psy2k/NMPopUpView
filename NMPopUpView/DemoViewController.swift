6cf9f413b713ac317775e6304d7ab625e8bf3e35
Nikos Maounis <nikospsy2k@gmail.com>
2014-09-13 19:51:35 +0300

    Swift demo view controller

---
+++ NMPopUpView/DemoViewController.swift
@@ -0,0 +1,58 @@
+//
+//  DemoViewController.swift
+//  NMPopUpView
+//
+//  Created by Nikos Maounis on 13/9/14.
+//  Copyright (c) 2014 Nikos Maounis. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+@objc class DemoViewController : UIViewController
+{
+    
+    @IBOutlet weak var showPopupBtn: UIButton!
+    
+    var popViewController : PopUpViewControllerSwift!
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(nibName nibNameOrNil: String!, bundle nibBundleOrNil: NSBundle!) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setRoundedBorder(5, withBorderWidth: 1, withColor: UIColor(red: 0.0, green: 122.0/2550, blue: 1.0, alpha: 1.0), forButton: showPopupBtn)
+    }
+    
+    @IBAction func showPopUp(sender: AnyObject) {
+        if (UIDevice.currentDevice().userInterfaceIdiom == .Pad)
+        {
+            self.popViewController = PopUpViewControllerSwift(nibName: "PopUpViewController_iPad", bundle: nil)
+            self.popViewController.title = "This is a popup view"
+            self.popViewController.showInView(self.view, withImage: UIImage(named: "typpzDemo"), withMessage: "You just triggered a great popup window", animated: true)
+        } else
+        {
+            self.popViewController = PopUpViewControllerSwift(nibName: "PopUpViewController", bundle: nil)
+            self.popViewController.title = "This is a popup view"
+            self.popViewController.showInView(self.view, withImage: UIImage(named: "typpzDemo"), withMessage: "You just triggered a great popup window", animated: true)
+        }
+    }
+    
+    
+    func setRoundedBorder(radius : CGFloat, withBorderWidth borderWidth: CGFloat, withColor color : UIColor, forButton button : UIButton)
+    {
+        let l : CALayer = button.layer
+        l.masksToBounds = true
+        l.cornerRadius = radius
+        l.borderWidth = borderWidth
+        l.borderColor = color.CGColor
+    }
+
+}
+
+
