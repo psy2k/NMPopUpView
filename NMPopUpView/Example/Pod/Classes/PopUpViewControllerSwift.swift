bd8e69831f06f65d4a2998e66aaa46faea603a54
Andrew Gerst <gerst20051@gmail.com>
2016-10-19 22:54:30 -0400

    swift 3 migration

---
+++ NMPopUpView/Example/Pod/Classes/PopUpViewControllerSwift.swift
@@ -0,0 +1,67 @@
+//
+//  PopUpViewControllerSwift.swift
+//  NMPopUpView
+//
+//  Created by Nikos Maounis on 13/9/14.
+//  Copyright (c) 2014 Nikos Maounis. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+@objc open class PopUpViewControllerSwift : UIViewController {
+    
+    @IBOutlet weak var popUpView: UIView!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var logoImg: UIImageView!
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override public init(nibName nibNameOrNil: String!, bundle nibBundleOrNil: Bundle!) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        self.popUpView.layer.cornerRadius = 5.0
+        self.popUpView.layer.shadowOpacity = 0.8
+        self.popUpView.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+    }
+    
+    open func showInView(_ aView: UIView!, withImage image: UIImage!, withMessage message: String!, animated: Bool) {
+        aView.addSubview(self.view)
+        logoImg!.image = image
+        messageLabel!.text = message
+        if animated {
+            self.showAnimate()
+        }
+    }
+    
+    func showAnimate() {
+        self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+        self.view.alpha = 0.0
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.alpha = 1.0
+            self.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        })
+    }
+    
+    func removeAnimate() {
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+            self.view.alpha = 0.0
+        }, completion: { (finished: Bool)  in
+            if finished {
+                self.view.removeFromSuperview()
+            }
+        })
+    }
+    
+    @IBAction open func closePopup(_ sender: AnyObject) {
+        self.removeAnimate()
+    }
+
+}
